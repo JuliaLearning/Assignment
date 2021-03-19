ab3cd3f9fc4b62247a19dc19b00e9ece6489bd9f
Jaan Tollander de Balsch <jaan@hey.com>
2021-03-19 10:55:58 +0200

    initla

---
+++ src/MultiPart.jl
@@ -0,0 +1,18 @@
+module MultiPart
+
+## Exercise 1
+# TODO: return "Multi"
+f() = nothing
+
+
+## Exercise 2
+# TODO: return "Part"
+g() = nothing
+
+
+## Exercise 2
+# TODO: return "Assignment"
+h() = nothing
+
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Test
+using MultiPart
+
+@test f() = "Multi"
+@test g() = "Part"
+@test h() = "Assignment"
